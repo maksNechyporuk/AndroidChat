876d7a502938c0f25d04da28bb94c23d93cdd9d0
MaksumNechiporuk <maksum.nec@gmail.com>
2020-10-09 10:58:41 +0200

    Images

--- app/src/main/java/com/example/simpleviber/ChatActivity.java
+++ app/src/main/java/com/example/simpleviber/ChatActivity.java
@@ -4,8 +4,12 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.text.Editable;
 import android.text.InputFilter;
 import android.text.TextWatcher;
@@ -19,17 +23,26 @@ import android.widget.ImageButton;
 import android.widget.ListView;
 import android.widget.ProgressBar;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.ChildEventListener;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.OnProgressListener;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
 import com.google.gson.Gson;
 import java.io.Console;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.UUID;
 
 public class ChatActivity extends AppCompatActivity {
     private ListView messageListView;
@@ -38,11 +51,13 @@ public class ChatActivity extends AppCompatActivity {
     private ImageButton sendImageButton;
     private Button sendMessageButton;
     private EditText messageEditText;
-
+    private final int PICK_IMAGE_REQUEST = 71;
     private String userName;
-
+    FirebaseStorage storage;
+    StorageReference storageReference;
     FirebaseDatabase database;
     DatabaseReference messagesDatabaseReference;
+    private Uri filePath;
 
 
     @Override
@@ -67,7 +82,8 @@ public class ChatActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
-
+        storage = FirebaseStorage.getInstance();
+        storageReference = storage.getReference();
         progressBar = findViewById(R.id.progressBar);
         sendImageButton = findViewById(R.id.sendPhotoButton);
         sendMessageButton = findViewById(R.id.sendMessageButton);
@@ -110,24 +126,65 @@ public class ChatActivity extends AppCompatActivity {
 
             }
         });
+
         messageEditText.setFilters(new InputFilter[]
                 {new InputFilter.LengthFilter(500)});
 
         sendMessageButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Message message = new Message();
-                message.setText(messageEditText.getText().toString());
-                message.setName(userName);
-                message.setImageUrl(null);
 
-                messagesDatabaseReference.push().setValue(message);
+                if(filePath != null)
+                {
+                    final ProgressDialog progressDialog = new ProgressDialog(ChatActivity.this);
+                    progressDialog.setTitle("Uploading...");
+                    progressDialog.show();
+
+                    StorageReference ref = storageReference.child("Images/"+ UUID.randomUUID().toString());
+                    ref.putFile(filePath)
+                            .addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                                @Override
+                                public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                                    progressDialog.dismiss();
+                                    Toast.makeText(ChatActivity.this, "Uploaded", Toast.LENGTH_SHORT).show();
+                                }
+                            })
+                            .addOnFailureListener(new OnFailureListener() {
+                                @Override
+                                public void onFailure(@NonNull Exception e) {
+                                    progressDialog.dismiss();
+                                    Toast.makeText(ChatActivity.this, "Failed "+e.getMessage(), Toast.LENGTH_SHORT).show();
+                                }
+                            })
+                            .addOnProgressListener(new OnProgressListener<UploadTask.TaskSnapshot>() {
+                                @Override
+                                public void onProgress(UploadTask.TaskSnapshot taskSnapshot) {
+                                    double progress = (100.0*taskSnapshot.getBytesTransferred()/taskSnapshot
+                                            .getTotalByteCount());
+                                    progressDialog.setMessage("Uploaded "+(int)progress+"%");
+                                }
+                            });
+
+                        sendMessageButton.setEnabled(false);
+
+                }else {
+                    Message message = new Message();
+                    message.setText(messageEditText.getText().toString());
+                    message.setName(userName);
+                    message.setImageUrl(null);
+
+                    messagesDatabaseReference.push().setValue(message);
+                }
+                messageEditText.setText("");
             }
         });
 
         sendImageButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+
+                chooseImage();
+
             }
         });
         messagesChildEventListener = new ChildEventListener() {
@@ -160,6 +217,32 @@ public class ChatActivity extends AppCompatActivity {
         };
 
         messagesDatabaseReference.addChildEventListener(messagesChildEventListener);
+    }
+    private void chooseImage() {
+        Intent intent = new Intent();
+        intent.setType("image/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(Intent.createChooser(intent, "Select Picture"), PICK_IMAGE_REQUEST);
+
+            sendMessageButton.setEnabled(true);
+
+    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode == PICK_IMAGE_REQUEST && resultCode == RESULT_OK
+                && data != null && data.getData() != null )
+        {
+            filePath = data.getData();
+            try {
+                Bitmap bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), filePath);
+
+            }
+            catch (IOException e)
+            {
+                e.printStackTrace();
+            }
+        }
     }
 
 }
